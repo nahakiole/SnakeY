e10120c48a97f48ec6acd353e2fbb056cb586495
Robin Glauser <robin.glauser@gmail.com>
2013-12-03 14:04:42 +0100

    Added Buffer

--- snake/src/Field.java
+++ snake/src/Field.java
@@ -2,24 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 
 public class Field extends JFrame implements ActionListener {
 
     public Snake snake = new Snake();
+    public BufferedImage Buffer = new BufferedImage(500,200,BufferedImage.TYPE_INT_ARGB);
 
     public Field(){
         Timer timer = new Timer(16,this);
         super.setSize(500,200);
         this.setLocationRelativeTo(getRootPane());
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         super.setVisible(true);
         repaint();
         timer.start();
     }
 
     public void paint(Graphics g){
+        Graphics g2 = Buffer.getGraphics();
 
-        g.clearRect(0,0,500,500);
-        snake.draw(g);
+        g2.setColor(new Color(255, 255, 255));
+        g2.clearRect(0,0,500,500);
+        g2.setColor(new Color(15, 75, 100));
+        snake.draw(g2);
+        g.drawImage(Buffer, 0, 0, null);
+        g.dispose();
     }
 
 
