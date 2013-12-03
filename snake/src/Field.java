f023ce5f6df0467188fd4ea3d2683ca4d3d2653b
Nahakiole <robin.glauser@gmail.com>
2013-12-03 14:44:43 +0100

    Added KeyListener

--- snake/src/Field.java
+++ snake/src/Field.java
@@ -2,27 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 public class Field extends JFrame implements ActionListener {
 
     public Snake snake = new Snake();
-    public BufferedImage Buffer = new BufferedImage(500,200,BufferedImage.TYPE_INT_ARGB);
+    public BufferedImage Buffer = new BufferedImage(500,400,BufferedImage.TYPE_INT_ARGB);
 
     public Field(){
         Timer timer = new Timer(16,this);
-        super.setSize(500,200);
+        super.setSize(500,400);
         this.setLocationRelativeTo(getRootPane());
         super.setDefaultCloseOperation(EXIT_ON_CLOSE);
         super.setVisible(true);
+        addKeyListener(new MyKeylistener());
         repaint();
         timer.start();
     }
 
+
+
     public void paint(Graphics g){
         Graphics2D g2 = Buffer.createGraphics();
         g2.setBackground(new Color(255,255,255));
-        g2.clearRect(0,0,500,500);
+        g2.clearRect(0,0,500,400);
         g2.setColor(new Color(15, 75, 100));
         snake.draw(g2);
         g.drawImage(Buffer, 0, 0, null);
@@ -34,5 +39,13 @@ public class Field extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         //System.out.println("Test");
         repaint();
+    }
+
+    public class MyKeylistener extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e) {
+            System.out.println(e.getKeyCode());
+            snake.direction = e.getKeyCode();
+        }
     }
 }
--- snake/src/Snake.java
+++ snake/src/Snake.java
@@ -1,12 +1,31 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Snake extends FieldObject {
-    private int i = 0;
+    private Point position = new Point(-40,80);
+    public int direction = KeyEvent.VK_RIGHT;
 
     @Override
     public void draw(Graphics g) {
-        i++;
-        g.fillRoundRect(i,80,40,40,0,0);
+
+        if (position.x > 500){
+            position.x = 0;
+        }
+        switch (direction){
+            case KeyEvent.VK_RIGHT:
+                position.x++; //Right
+                break;
+            case KeyEvent.VK_UP:
+                position.y--; //Down
+                break;
+            case KeyEvent.VK_LEFT:
+                position.x--; //Left
+                break;
+            case KeyEvent.VK_DOWN:
+                position.y++; //Up
+                break;
+        }
+        g.fillRoundRect(position.x,position.y,40,40,20,20);
         //To change body of implemented methods use File | Settings | File Templates.
     }
 }
